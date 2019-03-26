ea9f6bf9ba66543e02b5ea8cf3b1c797d8825df8
Jessica <woolard.jessica@gmail.com>
2019-03-26 11:47:33 -0400

    fixing jquery

--- petspace/static/js/main.js
+++ petspace/static/js/main.js
@@ -1,36 +1,45 @@
+(function ($) {
+    $(function () {
+        $(function () {
+            $('#form_signup_form').parsley();
 
-window.initMap = function (latitude, longitude) {
-    var latlng = new google.maps.LatLng(latitude, longitude);
-    let map = new google.maps.Map(document.getElementById('map'), {
-        center: latlng,
-        zoom: 11
-    });
-    var cityCircle = new google.maps.Circle({
+        });
+        window.initMap = function (latitude, longitude) {
+            var latlng = new google.maps.LatLng(latitude, longitude);
+            let map = new google.maps.Map(document.getElementById('map'), {
+                center: latlng,
+                zoom: 11
+            });
+            var cityCircle = new google.maps.Circle({
 
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35,
-        map: map,
-        center: latlng,
-        radius: 1500
-    });
-};
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#FF0000',
+                fillOpacity: 0.35,
+                map: map,
+                center: latlng,
+                radius: 1500
+            });
+        };
 
-function loadCoordinates() {
-    let user_id = $("#map").data("user-id");
-    $.ajax('/api/users/' + user_id, {
-        'method': 'GET',
-        'success': function (data) {
-            let latitude = data["latitude"];
-            let longitude = data["longitude"];
-            initMap(latitude, longitude)
-        },
-        'error': function (xhr) {
-            console.log(xhr.statusText);
+        function loadCoordinates() {
+            let user_id = $("#map").data("user-id");
+            $.ajax('/api/users/' + user_id, {
+                'method': 'GET',
+                'success': function (data) {
+                    let latitude = data["latitude"];
+                    let longitude = data["longitude"];
+                    initMap(latitude, longitude)
+                },
+                'error': function (xhr) {
+                    console.log(xhr.statusText);
+                }
+            });
         }
+
+        loadCoordinates();
+
     });
-}
-loadCoordinates();
+}(jQuery));
 
