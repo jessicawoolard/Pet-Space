138b60c356557eded12b151c566d78cbaa2e6b20
Jessica <woolard.jessica@gmail.com>
2019-03-27 22:09:54 -0400

    added README and django tests

--- petspace/tests.py
+++ petspace/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Pet
 
-# Create your tests here.
+
+class PetTestCase(TestCase):
+
+    def setUp(self):
+        Pet.objects.create(name='Cleo', type_of_animal='Sloth', description='Furry', hobbies='sleeping', lost=True)
+        Pet.objects.create(name='Sam', type_of_animal='Bird', description='Blue', hobbies='Flying', lost=False)
+
+    def test_pets_have_values(self):
+        sloth = Pet.objects.get(name='Cleo')
+        self.assertEqual(sloth.name, 'Cleo')
+        self.assertEqual(sloth.type_of_animal, 'Sloth')
+        self.assertEqual(sloth.description, 'Furry')
+        self.assertEqual(sloth.hobbies, 'Sleeping')
+        self.assertEqual(sloth.lost, True)
+
+        bird = Pet.objects.get(name='Sam')
+        self.assertEqual(bird.name, 'Sam')
+        self.assertEqual(bird.type_of_animal, 'Bird')
+        self.assertEqual(bird.description, 'Blue')
+        self.assertEqual(bird.hobbies, 'Flying')
+        self.assertEqual(bird.lost, False)
+
